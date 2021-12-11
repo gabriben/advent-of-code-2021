978b12486ee168f2881e4dd9d243c3c8ca66aeb5
Gabriel Bénédict <gbndict@gmail.com>
2021-12-11 13:27:42 +0100

    5* not working

---
+++ 2021/1/5.jl
@@ -0,0 +1,38 @@
+using DelimitedFiles
+using StaticArrays
+using Pipe
+using DataFrames
+using Underscores
+
+d = open("5.txt") do f readlines(f) end
+
+d = split.(d, " -> ")
+start = [i[1] for i in d]
+finish = [i[2] for i in d]
+start = @pipe start |> split.(_, ",") |>  hcat(_...) |> parse.(Int, _) |> transpose
+finish = @pipe finish |> split.(_, ",") |>  hcat(_...) |> parse.(Int, _) |> transpose
+d = DataFrame(hcat(start, finish), [:x1, :y1, :x2, :y2])
+
+xmax = maximum(hcat(d.x1, d.x2))
+ymax = maximum(hcat(d.y1, d.y2))
+
+grid = zeros(Int, xmax, ymax)
+
+straights = (d.x1 .== d.x2) .+ (d.y1 .== d.y2) .> 0
+d = d[straights, :]
+
+transform!(d,
+          [:x1, :x2] => max => :xmax,
+          [:x1, :x2] => min => :xmin,
+          [:y1, :y2] => max => :ymax,
+          [:y1, :y2] => min => :ymin
+          )
+
+for i in eachrow(d)
+    @views grid[i.xmin : i.xmax, i.ymin : i.ymax] .+= 1
+    # https://discourse.julialang.org/t/why-is-a-manual-in-place-addition-so-much-faster-than-and-on-range-indexed-arrays/3302
+    # @show grid[i.xmin : i.xmax, i.ymin : i.ymax]
+end
+
+sum(grid .> 1)
+
